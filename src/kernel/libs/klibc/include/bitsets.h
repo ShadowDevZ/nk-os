2554d40f8596e12a0decbf18c78cae820524a9e4
ShadowDevZ <splitvs2@gmail.com>
2023-12-25 09:57:09 +0100

    gdt macros

--- src/kernel/libs/klibc/include/bitsets.h
+++ src/kernel/libs/klibc/include/bitsets.h
@@ -8,4 +8,19 @@
 
 #define CHECK_MASK (x, y) (x & y)
 
+
+#define __LOEXT(x, y, b) ((uint ## b ## _t)(x) & (y))
+#define _HIEXT(x, y, b) ((uint ## b ## _t)((x) >> (8 * sizeof(x) - (b))) & (y))
+
+#define LOBYTE(x) __LOEXT(x, 0xFF, 8)
+#define HIBYTE(x) __HIEXT(x, 0xFF, 8)
+
+#define LOWORD(x) __LOEXT(x, 0xFFFF, 16)
+#define HIWORD(x) __HIEXT(x, 0xFFFF, 16)
+
+#define LODWORD(x) __LOEXT(x, 0xFFFFFFFF, 32)
+#define HIDWORD(x) __HIEXT(x, 0xFFFFFFFF, 32)
+
+
+
 bool IsValidFlag(uint32_t flag);
