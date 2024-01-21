d6c173d9b11fdd17cb24b0e40cdbd5a2dddd869d
ShadowDevZ <splitvs2@gmail.com>
2024-01-21 22:10:31 +0100

    shorter irq autogen macro

--- src/kernel/arch/x86_64/irq.asm
+++ src/kernel/arch/x86_64/irq.asm
@@ -1,34 +1,34 @@
 extern IRQ_Handler
 
-%macro X64_IRQ 2
+%macro X64_IRQ 1
 global X64_IRQ%1
 X64_IRQ%1:
     cld ; just in case the DF is set, clearing it is required by SYSVABI
     cli
     push rdi
-    mov rdi, %2 ;pass remapped address to the handler
+    mov rdi, %1+32 ;pass remapped address to the handler
     call IRQ_Handler
     pop rdi
     sti
     iretq
 %endmacro
 
-X64_IRQ 0, 32
-X64_IRQ 1, 33
-X64_IRQ 2, 34
-X64_IRQ 3, 35
-X64_IRQ 4, 36
-X64_IRQ 5, 37
-X64_IRQ 6, 38
-X64_IRQ 7, 39
-X64_IRQ 8, 40
-X64_IRQ 9, 41
-X64_IRQ 10, 42
-X64_IRQ 11, 43
-X64_IRQ 12, 44
-X64_IRQ 13, 45
-X64_IRQ 14, 46
-X64_IRQ 15, 47
+X64_IRQ 0
+X64_IRQ 1
+X64_IRQ 2
+X64_IRQ 3
+X64_IRQ 4
+X64_IRQ 5
+X64_IRQ 6
+X64_IRQ 7
+X64_IRQ 8
+X64_IRQ 9
+X64_IRQ 10
+X64_IRQ 11
+X64_IRQ 12
+X64_IRQ 13
+X64_IRQ 14
+X64_IRQ 15
 
 
 global irqHandlers
